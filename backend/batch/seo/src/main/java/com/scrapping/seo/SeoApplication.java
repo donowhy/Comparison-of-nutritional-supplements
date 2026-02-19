bcf39ea67644307741731caadbee4885ef5b50ce
강민승 <k4minseung@gmail.com>
2026-02-19 16:26:09 +0900

    fix | Hiding important values

--- backend/batch/seo/src/main/java/com/scrapping/seo/SeoApplication.java
+++ backend/batch/seo/src/main/java/com/scrapping/seo/SeoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SeoApplication.class, args);
     }
-
 }
